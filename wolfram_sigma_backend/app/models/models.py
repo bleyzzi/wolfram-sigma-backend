8727765be3353c86cbbc96bf113f6f8459c72474
Denis Borychev <denisborycev@gmail.com>
2024-03-21 20:14:15 +0300

    table params added

--- wolfram_sigma_backend/app/models/models.py
+++ wolfram_sigma_backend/app/models/models.py
@@ -10,13 +10,21 @@ from sqlalchemy.orm import Mapped, mapped_column
 from wolfram_sigma_backend.app.database.database import Base
 
 
+class Params(Base):
+    __tablename__ = "params"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    language: Mapped[str] = mapped_column(String, nullable=False)
+    threads: Mapped[int] = mapped_column(Integer, default=1, nullable=False)
+
+
 class Equation(Base):
     __tablename__ = "equation"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    expression: Mapped[str]
-    result: Mapped[str]
-    params: Mapped[str]
+    expression: Mapped[str] = mapped_column(String, nullable=False)
+    result: Mapped[str] = mapped_column(String, nullable=False)
+    params: Mapped[int] = mapped_column(Integer, ForeignKey("params.id"))
     created_at: Mapped[Optional[datetime]] = mapped_column(default=datetime.utcnow)
 
 

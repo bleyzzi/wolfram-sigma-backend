19761608777d2c3c72ffb559374c3030753a95a7
Danila Kryvanos <danila.krn@gmail.com>
2024-04-08 20:08:41 +0300

    change middleware options and pre-commit all project

--- wolfram_sigma_backend/app/auth/models.py
+++ wolfram_sigma_backend/app/auth/models.py
@@ -1,15 +1,9 @@
 import uuid
 from datetime import datetime
-from typing import Optional
 
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTableUUID
-from sqlalchemy import Boolean
-from sqlalchemy import ForeignKey
-from sqlalchemy import Integer
-from sqlalchemy import String
-from sqlalchemy import types
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
+from sqlalchemy import Boolean, ForeignKey, Integer, String, types
+from sqlalchemy.orm import Mapped, mapped_column
 
 from wolfram_sigma_backend.app.database.database import Base
 
@@ -19,18 +13,16 @@ class Role(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str]
-    permission: Mapped[Optional[str]]
+    permission: Mapped[str | None]
 
 
 class User(SQLAlchemyBaseUserTableUUID, Base):
     __tablename__ = "user"
 
-    id: Mapped[uuid.UUID] = mapped_column(
-        types.Uuid, primary_key=True, default=uuid.uuid4
-    )
+    id: Mapped[uuid.UUID] = mapped_column(types.Uuid, primary_key=True, default=uuid.uuid4)
     email: Mapped[str]
     username: Mapped[str]
-    registered_at: Mapped[Optional[datetime]] = mapped_column(default=datetime.utcnow)
+    registered_at: Mapped[datetime | None] = mapped_column(default=datetime.utcnow)
     role_id: Mapped[int] = mapped_column(Integer, ForeignKey("role.id"))
     hashed_password: Mapped[str] = mapped_column(String(length=1024), nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)

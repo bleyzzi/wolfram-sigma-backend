3f07e836877904645fde41603a94c4dc0bec3219
Danila Kryvanos <danila.krn@gmail.com>
2024-04-08 20:29:40 +0300

    add origins list to middleware

--- main.py
+++ main.py
@@ -26,9 +26,13 @@ app.include_router(
     tags=["auth"],
 )
 
+origins = [
+    "http://localhost:8000"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=[
